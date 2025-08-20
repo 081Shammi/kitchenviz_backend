2c3ec274efd3679692dcfb743a64ad4297595bcc
gcnaveen <naveengc99@gmail.com>
2025-08-20 20:14:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4050;
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+
+
+
+app.use(cors());
+
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+const API_ROOT ='/' 
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js backend!');
+  });
+  
+  
+  
+  // Database connection
+  try {
+      const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/Membership";
+      const DB_PORT = process.env.PORT || PORT;
+  
+      mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+          .then(() => {
+              console.log("DB Connection Successful");
+              app.listen(DB_PORT, () => {
+                  console.log(`Server is running on port ${DB_PORT}`);
+              });
+          })
+          .catch(err => {
+              console.log("Error in connecting to DB:", err);
+          });
+  } catch (error) {
+      console.log("Error in connecting to DB:", error);
+  }
+  
